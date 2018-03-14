30ab2ec76c6dfe2b7203567fe2f51d30fb28d49f
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2018-03-14 16:28:30 +0000

    Database message
    Sent message to database

--- app/src/main/java/com/openchat/openchat/MainActivity.java
+++ app/src/main/java/com/openchat/openchat/MainActivity.java
@@ -1,52 +1,39 @@
 package com.openchat.openchat;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editMessage;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        editMessage = findViewById(R.id.editMsg);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("messages");
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void buttonClicked(View view) {
+        final String message = editMessage.getText().toString().trim();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if(!TextUtils.isEmpty(message))
+        {
+            final DatabaseReference newPost = mDatabase.push();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            newPost.child("content").setValue(message);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
